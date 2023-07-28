5c5d0aa9220f228f915b9b8000908a15aa1b30c4
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-28 11:47:50 +0530

    pagination

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -28,6 +28,12 @@ const getAllProducts = asyncHandler(async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 });
