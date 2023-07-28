267b0ad47eb3b463afdbdf1869539eeb376024f7
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-28 11:02:39 +0530

    filter and search

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,22 @@
 import asyncHandler from "express-async-handler";
+import Product from "../models/productsModel.js";
 
 const getAllProducts = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: "Products route" });
+  const { featured, company, name } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObj);
+  const products = await Product.find(queryObj);
+  res.status(200).json({ products, nbHits: products.length });
 });
 
 export { getAllProducts };
